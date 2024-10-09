d6db7e1056ab88c112a19425274f6831a3331cde
Dayitva <dayitvagoel@gmail.com>
2024-10-09 21:29:17 +0700

    feat: async flow to torex

--- monorepo/contracts/superapp/src/SBIncentivesApp.sol
+++ monorepo/contracts/superapp/src/SBIncentivesApp.sol
@@ -3,34 +3,56 @@ pragma solidity ^0.8.0;
 
 import { SuperTokenV1Library } from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
 import { CFASuperAppBase } from "@superfluid-finance/ethereum-contracts/contracts/apps/CFASuperAppBase.sol";
-import { ISuperfluid, ISuperToken, ISuperfluidPool, PoolConfig } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import { ISuperfluid, ISuperToken, ISuperfluidPool, ISuperApp, PoolConfig, IERC20 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 // import { ISuperfluidPool } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluidPool.sol";
 import { ISETH } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/tokens/ISETH.sol";
 // import { PoolConfig } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/gdav1/IGeneralDistributionAgreementV1.sol";
 import { ITorex } from "./interfaces/ITorex.sol";
 import { Scaler } from "./utils/Scaler.sol";
 import { toInt96, UINT_100PCT_PM, INT_100PCT_PM } from "./utils/MathExtra.sol";
+import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
 
 contract SBIncentivesApp is CFASuperAppBase {
     using SuperTokenV1Library for ISuperToken;
     using SuperTokenV1Library for ISETH;
 
-    Scaler outPoolScaler;
+    address public ADMIN;
 
     address public torexAddr;
     ITorex public torex;
 
+    ISuperfluid host;
+
     ISuperToken public immutable inToken;
     ISuperToken public immutable outToken;
+    ISuperToken public immutable rewardToken;
 
-    PoolConfig private poolConfig;
-    ISuperfluidPool outPool;
+    PoolConfig private outPoolConfig;
+    PoolConfig private rewardPoolConfig;
+    ISuperfluidPool internal immutable outPool;
+    ISuperfluidPool internal immutable rewardPool;
+
+    Scaler internal immutable outPoolScaler;
+    Scaler internal immutable rewardPoolScaler;
+
+    event FlowCreated(address indexed sender, int96 flowRate);
+    event FlowUpdated(address indexed sender, int96 inflowChange);
+    event FlowDeleted(address indexed sender, int96 remainingInflow);
 
     constructor(
         ISuperfluid host_,
-        address _torexAddr
+        address _torexAddr,
+        ISuperToken _rewardToken,
+        address admin
     ) CFASuperAppBase (host_) {
-        
+        // host_.registerApp(getConfigWord(true, true, true));
+        selfRegister(true, true, true);
+
+        ADMIN = admin;
+
+        host = host_;
+
         torexAddr = _torexAddr;
         torex = ITorex(_torexAddr);
         (inToken, outToken) = ITorex(torexAddr).getPairedTokens();
@@ -38,10 +60,28 @@ contract SBIncentivesApp is CFASuperAppBase {
         ISuperfluidPool pool = torex.outTokenDistributionPool();
         // ACCEPTED_SUPER_TOKEN.connectPool(pool);
 
-        poolConfig.transferabilityForUnitsOwner = false;
-        poolConfig.distributionFromAnyAddress = false;
+        rewardToken = _rewardToken;
 
-        outPool = outToken.createPool(address(this), poolConfig);
+        outPoolConfig.transferabilityForUnitsOwner = false;
+        outPoolConfig.distributionFromAnyAddress = false;
+
+        rewardPoolConfig.transferabilityForUnitsOwner = false;
+        rewardPoolConfig.distributionFromAnyAddress = false;
+
+        outPool = outToken.createPool(address(this), outPoolConfig);
+        rewardPool = rewardToken.createPool(address(this), rewardPoolConfig);
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == ADMIN, "SBIncentivesApp: only admin");
+        _;
+    }
+
+    function approveSpending(uint256 upgradeAmount) external {
+        IERC20 underlyingToken = IERC20(inToken.getUnderlyingToken());
+        underlyingToken.approve(address(inToken), type(uint256).max);
+        inToken.upgrade(upgradeAmount);
+        inToken.approve(torexAddr, type(uint256).max);
     }
 
     /// @dev checks that only the acceptedToken is used when sending streams into this contract
@@ -57,33 +97,56 @@ contract SBIncentivesApp is CFASuperAppBase {
     ) internal override returns (bytes memory newCtx) {
         newCtx = ctx_;
 
-        require(isAcceptedSuperToken(superToken_));
-
         // get inflow rate from sender_
         int96 inflowRate = superToken_.getFlowRate(sender_, address(this));
 
-        // pool.updateMemberUnits(outPool, sender_, inflowRate/scaler);
+        //Scale the value from int96 to uint128 correctly
+        uint128 newUnits = uint128(SafeCast.toInt128(outPoolScaler.scaleValue(inflowRate)));
 
-        // if there's no outflow already, create outflows
-        if (superToken_.getFlowRate(address(this), torexAddr) == 0) {
-            newCtx = superToken_.createFlowWithCtx(
-                torexAddr,
-                inflowRate,
-                newCtx
-            );
-        }
+        outPool.updateMemberUnits(sender_, newUnits);
+        rewardPool.updateMemberUnits(sender_, newUnits);
 
-        // otherwise, there's already outflows which should be increased
-        else {
-            newCtx = superToken_.updateFlowWithCtx(
-                torexAddr,
-                superToken_.getFlowRate(address(this), torexAddr) +
-                    inflowRate,
-                newCtx
-            );
-        }
+        emit FlowCreated(sender_, inflowRate);
 
-        // TODO: Create a stream for rewards token to the user
+        // host.allowCompositeApp(ISuperApp(torexAddr));
+
+        // // if there's no outflow already, create outflows
+        // if (superToken_.getFlowRate(address(this), torexAddr) == 0) {
+        //     newCtx = superToken_.createFlowWithCtx(
+        //         torexAddr,
+        //         inflowRate,
+        //         newCtx
+        //     );
+        // }
+
+        // // otherwise, there's already outflows which should be increased
+        // else {
+        //     newCtx = superToken_.updateFlowWithCtx(
+        //         torexAddr,
+        //         superToken_.getFlowRate(address(this), torexAddr) +
+        //             inflowRate,
+        //         newCtx
+        //     );
+        // }
+
+        // // TODO: Do we create a stream of rewards token to the user or just assign units to the Pool?
+        // if (rewardToken.getFlowRate(address(this), sender_) == 0) {
+        //     newCtx = rewardToken.createFlowWithCtx(
+        //         sender_,
+        //         inflowRate,
+        //         newCtx
+        //     );
+        // }
+
+        // // otherwise, there's already outflows which should be increased
+        // else {
+        //     newCtx = superToken_.updateFlowWithCtx(
+        //         sender_,
+        //         rewardToken.getFlowRate(address(this), sender_) +
+        //             inflowRate,
+        //         newCtx
+        //     );
+        // }
 
     }
 
@@ -97,20 +160,32 @@ contract SBIncentivesApp is CFASuperAppBase {
         newCtx = ctx_;
 
         // get inflow rate change from sender_
-        int96 inflowChange = superToken_.getFlowRate(sender_, address(this)) - previousFlowRate_;
+        int96 currentFlowRate = superToken_.getFlowRate(sender_, address(this));
+        int96 inflowChange = currentFlowRate - previousFlowRate_;
+        
+        // Claim the tokens before assigning the new units
+        outPool.claimAll(sender_);
+        rewardPool.claimAll(sender_);
 
-        // update outflows
-        newCtx = superToken_.updateFlowWithCtx(
-            torexAddr,
-            superToken_.getFlowRate(address(this), torexAddr)
-                + inflowChange,
-            newCtx
-        );
+        //Scale the value from int96 to uint128 correctly
+        uint128 newUnits = uint128(SafeCast.toInt128(outPoolScaler.scaleValue(currentFlowRate)));
+
+        outPool.updateMemberUnits(sender_, newUnits);
+        rewardPool.updateMemberUnits(sender_, newUnits);
+
+        emit FlowUpdated(sender_, inflowChange);
+
+        // // update outflows
+        // newCtx = superToken_.updateFlowWithCtx(
+        //     torexAddr,
+        //     superToken_.getFlowRate(address(this), torexAddr) + inflowChange,
+        //     newCtx
+        // );
     }
 
     function onFlowDeleted(
         ISuperToken superToken_,
-        address, /*sender_*/
+        address sender_,
         address receiver_,
         int96 previousFlowRate_,
         uint256, /*lastUpdated*/
@@ -121,24 +196,65 @@ contract SBIncentivesApp is CFASuperAppBase {
         // remaining inflow is equal to total outflow less the inflow that just got deleted
         int96 remainingInflow = superToken_.getFlowRate(address(this), torexAddr) - previousFlowRate_;
 
-        // handle "rogue recipients" with sticky stream - see readme
-        if (receiver_ == torexAddr) {
-            newCtx = superToken_.createFlowWithCtx(receiver_, previousFlowRate_, newCtx);
+        // Claim all the tokens before updating the units
+        outPool.claimAll(sender_);
+        rewardPool.claimAll(sender_);
+        
+        // update outPool and rewardPool units
+        outPool.updateMemberUnits(sender_, 0);
+        rewardPool.updateMemberUnits(sender_, 0);
+
+        emit FlowDeleted(sender_, remainingInflow);
+
+        // // handle "rogue recipients" with sticky stream - see readme
+        // if (receiver_ == torexAddr) {
+        //     newCtx = superToken_.createFlowWithCtx(receiver_, previousFlowRate_, newCtx);
+        // }
+
+        // // if there is no more inflow, outflows should be deleted
+        // if (remainingInflow <= 0) {
+        //     newCtx = superToken_.deleteFlowWithCtx(address(this), torexAddr, newCtx);
+        // }
+        // // otherwise, there's still inflow left and outflows must be updated
+        // else {
+        //     newCtx = superToken_.updateFlowWithCtx(torexAddr, remainingInflow, newCtx);
+        // }
+    }
+
+    function createStreamToTorex(int96 flowRate) external onlyAdmin {
+        int96 currentOutFlowRate = inToken.getFlowRate(address(this), torexAddr);
+        
+        // if there's no outflow already, create outflows
+        if (currentOutFlowRate == 0) {
+            inToken.createFlow(torexAddr, flowRate);
         }
 
-        // if there is no more inflow, outflows should be deleted
-        if (remainingInflow <= 0) {
-            newCtx = superToken_.deleteFlowWithCtx(address(this), torexAddr, newCtx);
-        }
-        // otherwise, there's still inflow left and outflows must be updated
+        // otherwise, there's already outflows which should be increased
         else {
-            newCtx = superToken_.updateFlowWithCtx(torexAddr, remainingInflow, newCtx);
+            inToken.updateFlow(torexAddr, currentOutFlowRate + flowRate);
         }
     }
 
-    function claimOutTokens() external {
-        // TODO: Maybe add incentives for users to call this
+    function updateStreamToTorex(int96 inflowChange) external onlyAdmin {
+        // update outflows
+        inToken.updateFlow(torexAddr, inToken.getFlowRate(address(this), torexAddr) + inflowChange);
+    }
+
+    function deleteStreamToTorex(int96 remainingInflow) external onlyAdmin {
+        // if there is no more inflow, outflows should be deleted
+        if (remainingInflow <= 0) {
+            inToken.deleteFlow(address(this), torexAddr);
+        }
+        // otherwise, there's still inflow left and outflows must be updated
+        else {
+            inToken.updateFlow(torexAddr, remainingInflow);
+        }
+    }
+
+    function claimAllTokens() external {
         outToken.distributeToPool(address(this), outPool, outToken.balanceOf(address(this)));
+        outPool.claimAll(msg.sender);
+        rewardPool.claimAll(msg.sender);
     }
 }
 
--- monorepo/contracts/superapp/test/SBIncentivesApp.t.sol
+++ monorepo/contracts/superapp/test/SBIncentivesApp.t.sol
@@ -10,6 +10,7 @@ import { SuperTokenV1Library } from "@superfluid-finance/ethereum-contracts/cont
 import { ITorex } from "../src/interfaces/ITorex.sol";
 import { SBIncentivesApp } from "../src/SBIncentivesApp.sol";
 import { console } from "forge-std/console.sol";
+import { CFASuperAppBase } from "@superfluid-finance/ethereum-contracts/contracts/apps/CFASuperAppBase.sol";
 
 using SuperTokenV1Library for ISuperToken;
 
@@ -22,9 +23,14 @@ using SuperTokenV1Library for ISuperToken;
 contract SBIncentivesAppTest is Test {
     ISuperfluid host;
     ITorex torex;
+    ITorex torex2;
     ISuperToken inToken;
+    ISuperToken inToken2;
     ISuperToken outToken;
+    ISuperToken outToken2;
+    ISuperToken rewardToken;
     address alice = address(0x721);
+    address bob = address(0x722);
     int96 constant DEFAULT_FLOWRATE = 4 ether;
     address DEFAULT_DISTRIBUTOR = address(0x69);
     address DEFAULT_REFERRER = address(0x70);
@@ -36,17 +42,27 @@ contract SBIncentivesAppTest is Test {
         host = ISuperfluid(hostAddr);
         address torexAddr = vm.envAddress("CELOCUSD_TOREX1");
         torex = ITorex(torexAddr);
+
+        address torexAddr2 = vm.envAddress("CELOWETH_TOREX1");
+        torex2 = ITorex(torexAddr2);
     }
 
     function setUp() public {
         (inToken, outToken) = torex.getPairedTokens();
+        (inToken2, outToken2) = torex2.getPairedTokens();
+        rewardToken = inToken2;
+
         // can't directly deal SuperTokens (see https://github.com/foundry-rs/forge-std/issues/570), thus using upgrade()
         address underlyingToken = inToken.getUnderlyingToken();
-        deal(underlyingToken, alice, 150000 ether); // make her a trillionaire
+        deal(underlyingToken, alice, 1500000 ether); // make her a trillionaire
+
+        address underlyingToken2 = inToken2.getUnderlyingToken();
+        deal(underlyingToken2, alice, 150000 ether);// make her a trillionaire
     }
 
-    function testWithExactUpgradeAmount() public {
-        SBIncentivesApp app = new SBIncentivesApp(host, address(torex));
+    function aWithExactUpgradeAmount() public {
+        SBIncentivesApp app = new SBIncentivesApp(host, address(torex), rewardToken, bob);
+        // host.registerApp(app, 4294967297);
 
         vm.startPrank(alice);
 
@@ -59,6 +75,11 @@ contract SBIncentivesAppTest is Test {
         address underlyingToken = inToken.getUnderlyingToken();
         IERC20(underlyingToken).approve(address(inToken), type(uint256).max);
         inToken.upgrade(upgradeAmount);
+
+        uint256 upgradeAmount2 = 120000 ether;
+        address underlyingToken2 = inToken2.getUnderlyingToken();
+        IERC20(underlyingToken2).approve(address(inToken2), type(uint256).max);
+        inToken2.upgrade(upgradeAmount2);
 
         console.log(address(inToken));
         console.log(inToken.balanceOf(alice));
@@ -85,4 +106,132 @@ contract SBIncentivesAppTest is Test {
         assertEq(inToken.getFlowRate(address(app), address(torex)), DEFAULT_FLOWRATE, "wrong flowrate to torex");
         assertEq(uBalanceBefore - uBalanceAfter, underlyingAmount, "wrong underlying token amount after upgrade");
     }
-}
+
+    function testCreateStreamToTorex() public {
+        SBIncentivesApp app = new SBIncentivesApp(host, address(torex), rewardToken, bob);
+
+        vm.startPrank(alice);
+
+        uint256 upgradeAmount = 1200000 ether;
+        address underlyingToken = inToken.getUnderlyingToken();
+        IERC20(underlyingToken).approve(address(inToken), type(uint256).max);
+        inToken.upgrade(upgradeAmount);
+
+        deal(underlyingToken, address(app), 1500000 ether);
+        app.approveSpending(upgradeAmount);
+        
+        console.log(address(inToken));
+        console.log(inToken.balanceOf(alice));
+        console.log(inToken.getUnderlyingToken());
+        console.log(IERC20(inToken.getUnderlyingToken()).balanceOf(alice));
+
+        console.log(address(inToken));
+        console.log(inToken.balanceOf(address(app)));
+        console.log(inToken.getUnderlyingToken());
+        console.log(IERC20(inToken.getUnderlyingToken()).balanceOf(address(app)));
+
+        (uint256 underlyingAmount,) = inToken.toUnderlyingAmount(upgradeAmount);
+        uint256 uBalanceBefore = inToken.balanceOf(alice);
+        inToken.createFlow(address(app), DEFAULT_FLOWRATE);
+        uint256 uBalanceAfter = inToken.balanceOf(alice);
+
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        app.createStreamToTorex(DEFAULT_FLOWRATE);
+        vm.stopPrank();
+
+        console.log("uBalanceBefore", uBalanceBefore);
+        console.log("uBalanceAfter", uBalanceAfter);
+        console.log("underlyingAmount", underlyingAmount);
+
+        assertEq(inToken.getFlowRate(alice, address(app)), DEFAULT_FLOWRATE, "wrong flowrate to app");
+        assertEq(inToken.getFlowRate(address(app), address(torex)), DEFAULT_FLOWRATE, "wrong flowrate to torex");
+    }
+
+    function testUpdateStreamToTorex() public {
+        SBIncentivesApp app = new SBIncentivesApp(host, address(torex), rewardToken, bob);
+
+        vm.startPrank(alice);
+
+        uint256 upgradeAmount = 1200000 ether;
+        address underlyingToken = inToken.getUnderlyingToken();
+        IERC20(underlyingToken).approve(address(inToken), type(uint256).max);
+        inToken.upgrade(upgradeAmount);
+
+        deal(underlyingToken, address(app), 1500000 ether);
+        app.approveSpending(upgradeAmount);
+
+        (uint256 underlyingAmount,) = inToken.toUnderlyingAmount(upgradeAmount);
+        uint256 uBalanceBefore = inToken.balanceOf(alice);
+        inToken.createFlow(address(app), DEFAULT_FLOWRATE);
+        uint256 uBalanceAfter = inToken.balanceOf(alice);
+
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        app.createStreamToTorex(DEFAULT_FLOWRATE);
+        vm.stopPrank();
+
+        console.log("uBalanceBefore", uBalanceBefore);
+        console.log("uBalanceAfter", uBalanceAfter);
+        console.log("underlyingAmount", underlyingAmount);
+
+        assertEq(inToken.getFlowRate(alice, address(app)), DEFAULT_FLOWRATE, "wrong flowrate to app");
+        assertEq(inToken.getFlowRate(address(app), address(torex)), DEFAULT_FLOWRATE, "wrong flowrate to torex");
+
+        vm.startPrank(alice);
+        inToken.updateFlow(address(app), DEFAULT_FLOWRATE + 1 ether);
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        app.updateStreamToTorex(1 ether);
+        vm.stopPrank();
+
+        assertEq(inToken.getFlowRate(alice, address(app)), DEFAULT_FLOWRATE + 1 ether, "wrong flowrate to app");
+        assertEq(inToken.getFlowRate(address(app), address(torex)), DEFAULT_FLOWRATE + 1 ether, "wrong flowrate to torex");
+    }
+
+    function testDeleteStreamToTorex() public {
+        SBIncentivesApp app = new SBIncentivesApp(host, address(torex), rewardToken, bob);
+
+        vm.startPrank(alice);
+
+        uint256 upgradeAmount = 1200000 ether;
+        address underlyingToken = inToken.getUnderlyingToken();
+        IERC20(underlyingToken).approve(address(inToken), type(uint256).max);
+        inToken.upgrade(upgradeAmount);
+
+        deal(underlyingToken, address(app), 1500000 ether);
+        app.approveSpending(upgradeAmount);
+
+        (uint256 underlyingAmount,) = inToken.toUnderlyingAmount(upgradeAmount);
+        uint256 uBalanceBefore = inToken.balanceOf(alice);
+        inToken.createFlow(address(app), DEFAULT_FLOWRATE);
+        uint256 uBalanceAfter = inToken.balanceOf(alice);
+
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        app.createStreamToTorex(DEFAULT_FLOWRATE);
+        vm.stopPrank();
+
+        console.log("uBalanceBefore", uBalanceBefore);
+        console.log("uBalanceAfter", uBalanceAfter);
+        console.log("underlyingAmount", underlyingAmount);
+
+        assertEq(inToken.getFlowRate(alice, address(app)), DEFAULT_FLOWRATE, "wrong flowrate to app");
+        assertEq(inToken.getFlowRate(address(app), address(torex)), DEFAULT_FLOWRATE, "wrong flowrate to torex");
+
+        vm.startPrank(alice);
+        inToken.deleteFlow(alice, address(app));
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        app.deleteStreamToTorex(0);
+        vm.stopPrank();
+
+        assertEq(inToken.getFlowRate(alice, address(app)), 0, "wrong flowrate to app");
+        assertEq(inToken.getFlowRate(address(app), address(torex)), 0, "wrong flowrate to torex");
+    }
+} 
