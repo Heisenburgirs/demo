4bbe24d1304af5d975ff316f69c45b294518b5b8
heisenburgers <zkdaoorganization@gmail.com>
2024-11-06 17:14:20 +0400

    test

--- monorepo/webapp/src/apollo.js
+++
@@ -1,9 +0,0 @@
-// lib/apollo-client.js
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://base-mainnet.subgraph.x.superfluid.dev/', // Replace with your GraphQL API endpoint
-  cache: new InMemoryCache(),
-});
-
-export default client;
